ff0aee4b0450ccfa80531a806cd9e2446c96f2af
Brian Rosner <brosner@gmail.com>
2009-12-16 12:06:59 -0700

    added setup.py and an empty Python package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name = "django-contacts-import",
+    version = "0.1.0-dev1",
+    author = "Eldarion",
+    author_email = "development@eldarion.com",
+    description = "contact importing for Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/eldarion/django-contacts-import",
+    packages = [
+        "contacts_import",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
