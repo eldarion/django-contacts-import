3f50a9d299ee9c65d4085e1c1dca181ccb7e5270
Brian Rosner <brosner@gmail.com>
2009-12-17 06:52:55 -0700

    keeping it together

--- contacts_import/urls.py
+++ contacts_import/urls.py
@@ -3,14 +3,10 @@ from django.conf.urls.defaults import *
 import contacts_import.tasks
 
 
-urlpatterns = patterns("contacts_import.views",
-    url(r"^import_contacts/$", "import_contacts", name="import_contacts"),
-    url(r"^authsub/login/$", "authsub_login", name="authsub_login"),
-)
-
-
-urlpatterns += patterns("bbauth.views",
-    url(r"^bbauth/login/$", "login", {
-        "redirect_to": "/contacts/import_contacts/"
+urlpatterns = patterns("",
+    url(r"^import_contacts/$", "contacts_import.views.import_contacts", name="import_contacts"),
+    url(r"^authsub/login/$", "contacts_import.views.authsub_login", name="authsub_login"),
+    url(r"^bbauth/login/$", "bbauth.views.login", {
+        "redirect_to": "/contacts/import_contacts/",
     }, name="bbauth_login"),
 )
