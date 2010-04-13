0f529811d331351ded25f73b4c0ad97823528efe
Brian Rosner <brosner@gmail.com>
2010-04-13 11:10:03 -0600

    PEP 386 compliant version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-contacts-import",
-    version = "0.1.0-dev1",
+    version = "0.1.dev1",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "contact importing for Django",
