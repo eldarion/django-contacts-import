34536172689dcd9eda5aef356dee4366c0d5a2a7
Brian Rosner <brosner@gmail.com>
2010-04-13 15:06:13 -0600

    style changes

--- contacts_import/forms.py
+++ contacts_import/forms.py
@@ -8,6 +8,8 @@ class VcardImportForm(forms.Form):
     vcard_file = forms.FileField(label="vCard File")
     
     def save(self, user, runner_class=SynchronousRunner):
-        importer = runner_class(VcardImporter, user=user,
-            stream=self.cleaned_data["vcard_file"])
+        importer = runner_class(VcardImporter,
+            user = user,
+            stream = self.cleaned_data["vcard_file"]
+        )
         return importer.import_contacts()
