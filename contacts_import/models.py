9936e591cd62ce54bfd556053ec32ceef62c73eb
Brian Rosner <brosner@gmail.com>
2010-04-15 14:36:36 -0600

    renamed Contact to TransientContact and removed fields that don't belong

--- contacts_import/models.py
+++ contacts_import/models.py
@@ -5,16 +5,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Contact(models.Model):
+class TransientContact(models.Model):
     # The user who created this contact
     user = models.ForeignKey(User, related_name="contacts")
     
     name = models.CharField(max_length=100, blank=True)
     email = models.EmailField()
-    added = models.DateField(default=date.today)
-    
-    # the user(s) this contact ultimately corrosponds to
-    users = models.ManyToManyField(User)
     
     def __unicode__(self):
         return "%s (%s's contact)" % (self.email,  self.user)
