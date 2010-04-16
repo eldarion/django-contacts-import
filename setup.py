3a4e357762114d2e137c5f403711f477dda76a7e
Brian Rosner <brosner@gmail.com>
2010-04-15 21:31:42 -0600

    0.1.dev2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-contacts-import",
-    version = "0.1.dev1",
+    version = "0.1.dev2",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "contact importing for Django",
